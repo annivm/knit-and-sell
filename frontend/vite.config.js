cfa8cf572679b83dbc6a8b1d14475600c4600b27
Anni Mustonen <anni.mustonen@tuni.fi>
2025-04-25 15:11:13 +0300

    added plugins

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import compression from 'vite-plugin-compression';
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), compression()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.js',
+    coverage: {
+       reporter: ['text', 'json', 'html'],
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
+});
