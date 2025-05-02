eed68cc015f1880771979ae721e566782256e19c
Anni Mustonen <anni.mustonen@tuni.fi>
2025-05-02 14:22:56 +0300

    modified messages

--- backend/src/models/items.model.ts
+++ backend/src/models/items.model.ts
@@ -3,14 +3,14 @@ import { z } from "zod";
 export const itemSchema = z.object (
     {
         id: z.string(),
-        name: z.string().min(2, 'Name of the item must be at least 2 character'),
-        price: z.string().min(1, "Price must be at least 1 character"),
-        description: z.string().min(4, 'Description must be at least 4 character'),
+        name: z.string().min(2, 'Name of the item must be at least 2 character.'),
+        price: z.string().min(1, "Price must be at least 1 character."),
+        description: z.string().min(4, 'Description must be at least 4 character.'),
         material: z.string(),
         size: z.string(),
         color: z.string(),
         category: z.string(),
-        image: z.string().min(5, "Filename + extension").optional().or(z.literal('')),
+        image: z.string().min(5, "Filename + extension.").optional().or(z.literal('')),
         owner_id: z.string().uuid(),
         created: z.string(),
         updated: z.string(),
@@ -21,15 +21,15 @@ export type Item = z.infer<typeof itemSchema>
 
 export const itemCreateRequestSchema = z.object(
     {
-        name: z.string().min(2, "Name must be at least 2 character"),
-        price: z.string().min(1, "Price must be at least 1 character"),
-        description: z.string().min(4, "Description must be at least 4 character"),
+        name: z.string().min(2, "Name must be at least 2 character."),
+        price: z.string().min(1, "Price must be at least 1 character."),
+        description: z.string().min(4, "Description must be at least 4 character."),
         material: z.string().optional(),
         size: z.string().optional(),
         color: z.string().optional(),
         category: z.string().optional(),
         other: z.string().optional(),
-        image: z.string().min(5, "Filename + extension").optional().or(z.literal('')),
+        image: z.string().min(5, "Filename + extension.").optional().or(z.literal('')),
         owner_id: z.string().uuid()
     }
 )
@@ -43,15 +43,15 @@ export const itemByIdRequestSchema = z.coerce.number().int().positive()
 export const itemUpdateRequestSchema = z.object(
     {
         id: z.number().positive(),
-        name: z.string().min(2, "Name must be at least 2 character"),
-        price: z.string().min(1, "Price must be at least 1 character"),
-        description: z.string().min(4, "Description must be at least 4 character"),
+        name: z.string().min(2, "Name must be at least 2 character."),
+        price: z.string().min(1, "Price must be at least 1 character."),
+        description: z.string().min(4, "Description must be at least 4 character."),
         material: z.string().optional(),
         size: z.string().optional(),
         color: z.string().optional(),
         category: z.string().optional(),
         other: z.string().optional(),
-        image: z.string().min(5, "Filename + extension").optional().or(z.literal(''))
+        image: z.string().min(5, "Filename + extension.").optional().or(z.literal(''))
     }
   )
   export type ItemUpdateRequest = z.infer<typeof itemUpdateRequestSchema>
--- backend/src/models/users.model.ts
+++ backend/src/models/users.model.ts
@@ -3,8 +3,8 @@ import { z } from 'zod';
 const userSchema = z.object(
     {
         id: z.string(),
-        name: z.string().min(1, "Name of the user must be at least 1 character").max(100, "Name of the user must be less then 100 characters" ),
-        email: z.string().email("Must be a valid email"),
+        name: z.string().min(1, "Name of the user must be at least 1 character.").max(100, "Name of the user must be less then 100 characters." ),
+        email: z.string().email("Must be a valid email."),
         password: z.string(),
         created: z.string(),
         updated: z.string()
@@ -16,8 +16,8 @@ export type User = z.infer<typeof userSchema>;
 export const signUpUserSchema = z.object(
     {
         id: z.string().optional(),
-        name: z.string().min(1, "Name of the user must be at least 1 character").max(100, "Name of the user must be less then 100 characters" ),
-        email: z.string().email("Must be a valid email"),
+        name: z.string().min(1, "Name of the user must be at least 1 character.").max(100, "Name of the user must be less then 100 characters." ),
+        email: z.string().email("Must be a valid email."),
         password: z.string()
     }
 )
@@ -26,8 +26,8 @@ export type UserCreateRequest = z.infer<typeof signUpUserSchema>;
 
 export const loginUserRequestSchema = z.object(
     {
-        email: z.string().email("Must be a valid email"),
-        password: z.string().min(1, "Password must be at least 1 character")
+        email: z.string().email("Must be a valid email."),
+        password: z.string().min(1, "Password must be at least 1 character.")
     }
 )
 export type LoginUserRequest = z.infer<typeof loginUserRequestSchema>;
