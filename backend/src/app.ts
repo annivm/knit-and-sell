b0449e842bed6b918df1cce77626b00abb1d036c
Anni Mustonen <anni.mustonen@tuni.fi>
2025-05-06 21:00:45 +0300

    added uploadpath for creating folder for images

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,8 +3,11 @@ import cors from 'cors'
 import dotenv from 'dotenv';
 import itemRoutes from "./routes/item.route";
 import usersRoutes from "./routes/users.route";
+import path from "path";
+import fs from 'fs';
 
-
+const uploadPath = path.join(__dirname,'..', 'uploads', 'images');
+fs.mkdirSync(uploadPath, { recursive: true });
 
 dotenv.config();
 
